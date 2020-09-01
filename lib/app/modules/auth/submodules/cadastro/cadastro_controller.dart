36e341ce1f062f8f45b27ce8e254d069d45c3a71
Pastor <Leonardo_FRSilva@hotmail.com>
2020-09-01 11:11:48 -0300

    Init casdastro Commit

--- lib/app/modules/auth/submodules/cadastro/cadastro_controller.dart
+++ lib/app/modules/auth/submodules/cadastro/cadastro_controller.dart
@@ -7,11 +7,5 @@ part 'cadastro_controller.g.dart';
 class CadastroController = _CadastroControllerBase with _$CadastroController;
 
 abstract class _CadastroControllerBase with Store {
-  @observable
-  int value = 0;
-
-  @action
-  void increment() {
-    value++;
-  }
+  
 }
--- lib/app/modules/auth/submodules/cadastro/cadastro_page.dart
+++ lib/app/modules/auth/submodules/cadastro/cadastro_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'cadastro_controller.dart';
+import '../../../auth/utils/Colors.dart';
 
 class CadastroPage extends StatefulWidget {
   final String title;
@@ -14,14 +16,60 @@ class _CadastroPageState
     extends ModularState<CadastroPage, CadastroController> {
   //use 'controller' variable to access controller
 
+  //Visible or not Visible the Password
+  bool obscureText = true;
+  bool obscureTextConf = true;
+
+  Widget get _backGround {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        begin: AlignmentDirectional.topCenter,
+        end: AlignmentDirectional.bottomCenter,
+        colors: GRADIENT,
+        stops: [0.1, 0.3, 0.4, 0.7],
+      )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
+              _backGround,
+              Container(
+                height: double.infinity,
+                alignment: AlignmentDirectional.center,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: size.width * 0.075,
+                    // vertical: size.height * 0.10,
+                  ),
+                  child: Form(
+                    // key: controller.formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
