62c105277c65a4a9fa2e7a0b28e2fb6ff0f29c8f
Pastor <Leonardo_FRSilva@hotmail.com>
2020-09-01 12:09:26 -0300

    Update pages

--- lib/app/modules/auth/submodules/cadastro/cadastro_module.dart
+++ lib/app/modules/auth/submodules/cadastro/cadastro_module.dart
@@ -1,18 +1,19 @@
-import 'pages/dados_conta/dados_conta_controller.dart';
-import 'pages/dados_endereco/dados_endereco_controller.dart';
-import 'pages/dados_endereco_bloc.dart';
 import 'cadastro_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'cadastro_page.dart';
+import 'pages/dados_conta/dados_conta_controller.dart';
+import 'pages/dados_endereco/dados_endereco_controller.dart';
+import 'pages/dados_pessoais/dados_pessoais_controller.dart';
 
 class CadastroModule extends ChildModule {
   @override
   List<Bind> get binds => [
-        $DadosContaController,
-        $DadosEnderecoController,
-        $DadosEnderecoBloc,
         Bind((i) => CadastroController()),
+
+        Bind((i) => DadosPessoaisController()),
+        Bind((i) => DadosEnderecoController()),
+        Bind((i) => DadosContaController()),
       ];
 
   @override
--- lib/app/modules/auth/submodules/cadastro/pages/dados_pessoais/dados_pessoais_page.dart
+++ lib/app/modules/auth/submodules/cadastro/pages/dados_pessoais/dados_pessoais_page.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:modulo_login/app/modules/auth/submodules/cadastro/pages/dados_pessoais/dados_pessoais_controller.dart';
 
 class DadosPessoaisPage extends StatefulWidget {
   @override
   _DadosPessoaisPageState createState() => _DadosPessoaisPageState();
 }
 
-class _DadosPessoaisPageState extends State<DadosPessoaisPage> {
+class _DadosPessoaisPageState extends ModularState<DadosPessoaisPage, DadosPessoaisController> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
     );
   }
 }
